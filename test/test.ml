ec5cf772358cda0ee07326ef0ec9e9182b3f9778
Ximin Luo <infinity0@pwned.gg>
2016-07-15 04:49:26 +0200

    Refactor tests to contain fewer redundant names

--- test/test.ml
+++ test/test.ml
@@ -2,7 +2,7 @@ open OUnit2
 
 (** TODO: OUnit2 should detect test suites automatically. *)
 let all_suites = [
-	TestHello.suite;
-  ]
+  "Hello" >::: TestHello.suite;
+]
 
 let () = run_test_tt_main ("all" >::: all_suites)
